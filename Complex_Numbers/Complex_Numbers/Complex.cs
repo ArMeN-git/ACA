869c832a64b19715c99f1bdebb9a5b3826d43976
ArMeN-git <57056578+ArMeN-git@users.noreply.github.com>
2019-11-21 19:56:13 +0400

    Add files via upload

---
+++ Complex_Numbers/Complex_Numbers/Complex.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex_Numbers
+{
+    class Complex
+    {
+        public double RealPart { get; private set; }
+        public double ImaginaryPart { get; private set; }
+
+        public Complex(double real, double imaginary)
+        {
+            this.RealPart = real;
+            this.ImaginaryPart = imaginary;
+        }
+
+        // Addition of two complex numbers ...
+        public static Complex operator +(Complex n1, Complex n2)
+        {
+            Complex res = new Complex(n1.RealPart + n2.RealPart, n1.ImaginaryPart + n2.ImaginaryPart);
+            return res; 
+        }
+
+        // Subtraction of two complex numbers ...
+        public static Complex operator -(Complex n1, Complex n2)
+        {
+            Complex res = new Complex(n1.RealPart - n2.RealPart, n1.ImaginaryPart - n2.ImaginaryPart);
+            return res;
+        }
+
+        // Multiplication of two complex numbers ...
+        public static Complex operator *(Complex n1, Complex n2)
+        {
+            double real = n1.RealPart * n2.RealPart - n1.ImaginaryPart * n2.ImaginaryPart;
+            double imaginary = n1.ImaginaryPart * n2.RealPart + n1.RealPart * n2.ImaginaryPart;
+            Complex res = new Complex(real, imaginary);
+            return res;
+        }
+
+        // Division of two complex numbers ...
+        public static Complex operator /(Complex n1, Complex n2)
+        {
+            if(n2.RealPart !=0 && n2.ImaginaryPart != 0)
+            {
+                double real = (n1.RealPart * n2.RealPart + n1.ImaginaryPart * n2.ImaginaryPart) / (Math.Pow(n2.RealPart, 2) + Math.Pow(n2.ImaginaryPart, 2));
+                double imaginary = (n1.ImaginaryPart * n2.RealPart - n1.RealPart * n2.ImaginaryPart) / (Math.Pow(n2.RealPart, 2) + Math.Pow(n2.ImaginaryPart, 2));
+                Complex res = new Complex(real, imaginary);
+                return res;
+            }
+            Console.WriteLine("The 2-nd complex number cannot be zero");
+            return null;
+        }
+
+        // Absolute value of complex number ...
+        public double AbsoluteValue()
+        {
+            double z = Math.Sqrt(Math.Pow(this.RealPart, 2) + Math.Pow(this.ImaginaryPart, 2));
+            return z;
+        }
+
+        // Argument of complex number ...
+        public double Argument()
+        {
+            return Math.Atan2(this.ImaginaryPart, this.RealPart) * 180 / Math.PI;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.RealPart} + {this.ImaginaryPart}i \n";
+        }
+    }
+}
---
+++ Complex_Numbers/Complex_Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c1 = new Complex(3, 4);
+            Console.WriteLine(c1);
+            Complex c2 = new Complex(6, 7);
+            Console.WriteLine(c2);
+            Complex c3 = c1 + c2;
+            Console.WriteLine(c3);
+            c3 = c1 - c2;
+            Console.WriteLine(c3);
+            c3 = c1 * c2;
+            Console.WriteLine(c3);
+            c3 = c1 / c2;
+            Console.WriteLine(c3);
+            Console.WriteLine(c1.AbsoluteValue());
+            Console.WriteLine(c1.Argument());
+            Console.ReadKey();
+        }
+    }
+}
