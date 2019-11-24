0226fd50319c774d1c09532fd5b3476716e42cf8
ArMeN-git <57056578+ArMeN-git@users.noreply.github.com>
2019-11-24 22:11:19 +0400

    Added Multimap

---
+++ Multimap/Multimap/Multimap.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multimap
+{
+    class Multimap<T, V>
+    {
+        private Dictionary<T, List<V>> dictMult = new Dictionary<T, List<V>>();
+
+        public List<V> this[T key] {
+            get
+            {
+                List<V> list;
+                if(this.dictMult.TryGetValue(key, out list))
+                {
+                    return list;
+                }
+                else
+                {
+                    return new List<V>();
+                }
+            }
+        }
+
+        public ICollection<T> Keys => this.dictMult.Keys;
+
+        public ICollection<List<V>> Values => this.dictMult.Values;
+ 
+        public int Count => this.dictMult.Count();
+
+        public void Add(T key, V value)
+        {
+            List<V> list;
+            if (this.dictMult.TryGetValue(key, out list))
+            {
+                list.Add(value);
+            }
+            else
+            {
+                list = new List<V>();
+                list.Add(value);
+                this.dictMult[key] = list;
+            }
+        }
+
+        public bool ContainsKey(T key)
+        {
+            return dictMult.ContainsKey(key);
+        }
+
+        public bool TryGetValue(T key, out List<V> value)
+        {
+            return this.dictMult.TryGetValue(key, out value);
+        }
+    }
+}
---
+++ Multimap/Multimap/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multimap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Multimap<string, int> mt = new Multimap<string, int>();
+            mt.Add("test", 25);
+            mt.Add("test", 36);
+            mt.Add("test", 47);
+            mt.Add("some", 478);
+            mt.Add("some", 167);
+            mt.Add("other", 27);
+            List<int> vals = mt["some"];
+            foreach (var v in vals)
+                Console.WriteLine(v);
+            Console.WriteLine();
+            foreach (var k in mt.Keys)
+                Console.WriteLine(k);
+            Console.WriteLine();
+            Console.WriteLine(mt.Count);
+            Console.WriteLine();
+            Console.WriteLine(mt.ContainsKey("other"));
+            Console.WriteLine();
+            List<int> values;
+            Console.WriteLine(mt.TryGetValue("test", out values));
+            Console.WriteLine();
+            foreach (var v in values)
+                Console.WriteLine(v);
+            Console.WriteLine();
+            foreach (var list in mt.Values)
+                foreach (var v in list)
+                    Console.WriteLine(v);
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
