e135e3c4d43047d470b3dfe9c876601fa8d41661
ArMeN-git <armengrigoryan244@gmail.com>
2019-12-02 23:26:30 +0400

    Added Rolling A Dice

---
+++ RollingADice/RollingADice/Die.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollingADice
+{
+    class Die
+    {
+        private Random r;
+        private int[] nums;
+        public int SideCount { get; }
+
+        public delegate void DieHandler(string msg);
+
+        public event DieHandler TwoFours;
+        public event DieHandler SumOfRoles;
+
+        public Die()
+        {
+            nums = new int[5];
+            SideCount = 6;
+            r = new Random();
+        }
+
+        /// <summary>
+        /// Move the array 1 element back to always get the last digits
+        /// </summary>
+        /// <param name="arr"></param>
+        private void MoveArray(ref int[] arr)
+        {
+            for(int i = 1; i < arr.Length; i++)
+            {
+                arr[i - 1] = arr[i];
+            }
+        }
+
+        public void Roll()
+        {
+            int n = 0;
+            int count = 0;
+            int sum;
+            while (n < 50)
+            {
+                sum = 0;
+                int number = r.Next(1, SideCount + 1);
+                nums[nums.Length - 1] = number;
+                foreach (var t in nums)
+                {
+                    Console.Write(t + " ");
+                    sum += t;
+                }
+
+                if (nums[nums.Length - 2] == 4 && nums[nums.Length - 1] == 4)
+                {
+                    TwoFours?.Invoke("\t Two Fours in a row!");
+                    count++;
+                }
+
+                if (sum >= 20)
+                    SumOfRoles?.Invoke("\t" + sum + " Equal or Greather than 20");
+                else
+                    Console.WriteLine("\t" + sum + " Less than 20");
+
+                MoveArray(ref nums);
+                n++;
+            }
+            TwoFours?.Invoke($"Count of Two Fours: {count}");
+        }
+    }
+}
---
+++ RollingADice/RollingADice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollingADice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Die dice = new Die();
+            dice.TwoFours += GetEventsInformation;
+            dice.SumOfRoles += GetEventsInformation;
+            dice.Roll();
+            Console.ReadKey();
+        }
+
+        static void GetEventsInformation(string msg)
+        {
+            ConsoleColor prev = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(msg);
+            Console.ForegroundColor = prev;
+        }
+    }
+}
