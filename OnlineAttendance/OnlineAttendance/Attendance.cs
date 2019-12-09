e33bdfbd502c83185e426162c793aede835f1ea1
ArMeN-git <armengrigoryan244@gmail.com>
2019-12-10 01:05:34 +0400

    Added Online Attendance

---
+++ OnlineAttendance/OnlineAttendance/Attendance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineAttendance
+{
+    class Attendance
+    {
+        public event Action<string> CheckNameEvent;
+
+        public void RegisterUser(string name)
+        {
+            if (name == "Steven" || name == "Mathew" || name == "Jack")
+                CheckNameEvent?.Invoke("Please choose other username");
+            else
+                Console.WriteLine($"Welcome {name}");
+        }
+    }
+}
---
+++ OnlineAttendance/OnlineAttendance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineAttendance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your username: ");
+            string s = Console.ReadLine();
+            Attendance at = new Attendance();
+            at.CheckNameEvent += OnCheckName;
+            at.RegisterUser(s);
+            Console.ReadKey();
+        }
+
+        private static void OnCheckName(string obj)
+        {
+            Console.WriteLine(obj);
+        }
+    }
+}
